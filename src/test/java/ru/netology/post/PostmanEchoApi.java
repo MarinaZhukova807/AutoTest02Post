1c5eb1a56a3ef7b0593cfb91f2b785c92b7191f0
Марина Жукова <marinazhukova807@gmail.com>
2022-08-01 22:49:05 +0300

    first commit

---
+++ src/test/java/ru/netology/post/PostmanEchoApi.java
@@ -0,0 +1,41 @@
+package ru.netology.post;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoApi {
+    @Test
+    void shouldPostDataTest(){
+        // Given - When - Then
+        // Предусловия
+        given()
+        .baseUri("https://postman-echo.com")
+        .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+        .when()
+        .post("/post")
+        // Проверки
+        .then()
+        .statusCode(200)
+        .body("data", equalTo("some data"))
+        ;
+    }
+
+    @Test
+    void shouldPostTwoElementsDataTest(){
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("\"json\": {\"foo1\": \"bar1\",\"foo2\": \"bar2\"}") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body("form", equalTo("\"foo1\": \"bar1\",\"foo2\": \"bar2\""))
+        ;
+    }
+}
